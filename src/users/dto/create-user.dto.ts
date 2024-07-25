c47b0aa8c507d53f9296081ade8e4c485ec66ffd
Jaimito27 <softjaimito27@gmail.com>
2024-07-25 08:58:49 -0500

    Se crea DTIO users y se importa en el servicio

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  doc_type: string;
+  ident_document: number;
+  names: string;
+  last_name: string;
+  phone: string;
+  email: string;
+  username: string;
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = this.userRepository.create();
+    this.userRepository.save(newUser)
   }
 
   findAll() {
