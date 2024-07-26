b215aea230029d3de5e5b58194a374d04a701525
Jaimito27 <softjaimito27@gmail.com>
2024-07-25 23:09:09 -0500

    se crea CREATE - DTO PETS

--- src/pets/dto/create-pet.dto.ts
+++ src/pets/dto/create-pet.dto.ts
@@ -1,1 +1,38 @@
-export class CreatePetDto {}
+import {
+    IsBoolean,
+    IsIn,
+    IsNotEmpty,
+    IsString,
+    MinLength,
+  } from 'class-validator';
+  
+  export class CreatePetDto {
+    @IsString()
+    @MinLength(2)
+    @IsNotEmpty()
+    nombre: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    edad: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    tipo: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    raza: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    @IsBoolean()
+    peligros: boolean;
+  
+    @IsString()
+    @IsNotEmpty()
+    @IsIn(['Macho', 'Hembra']) //solo pueden ingresas estas 2 opciones
+    genero: string;
+  }
+  
