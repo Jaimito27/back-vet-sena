dc669fda2b8a295c7a783f34eed9514a91a92893
Jaimito27 <softjaimito27@gmail.com>
2024-07-31 12:05:43 -0500

    se crea entidad y DTO de citas

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,12 @@
-export class CreateAppointmentDto {}
+import { IsDate, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateAppointmentDto {
+  @IsDate()
+  @IsNotEmpty()
+  date_appointment: Date;
+
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
+  type_procedure: string;
+}
--- src/appointments/entities/appointment.entity.ts
+++ src/appointments/entities/appointment.entity.ts
@@ -1,1 +1,14 @@
-export class Appointment {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Appointment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({nullable: false})
+    date_appointment: Date;
+
+    @Column({nullable: false})
+    type_procedure: string;
+ 
+}
