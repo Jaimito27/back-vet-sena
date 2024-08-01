134980867c825108b946ed9824b76c2411d9af4a
Jaimito27 <softjaimito27@gmail.com>
2024-08-01 14:13:06 -0500

    Se genera constante de jwt, se realizan configuraciones globales y se emplea la generación del token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 import { LoginModule } from '../../src/login/login.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants/jwt.constant';
 
 @Module({
-  imports:[LoginModule],
+  imports:[LoginModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { AuthDto } from './dto/auth.dto';
 import { LoginService } from '../../src/login/login.service';
 import * as bcryptjs from 'bcryptjs';
 import { Login } from '../../src/login/entities/login.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly loginService: LoginService) {}
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly jwtService: JwtService
+) {}
 
   async login({ username, password }: AuthDto) {
     const userFound = await this.loginService.getUserUsername(username);
@@ -26,6 +30,13 @@ export class AuthService {
     if (!isPasswordValid)
       return new HttpException('Contraseña invalida', HttpStatus.UNAUTHORIZED);
 
-    return 'login';
+
+
+    const patyload = {username: userFound.username}
+    const token = await this.jwtService.signAsync(patyload)
+
+    return {
+        token, username
+    };
   }
 }
