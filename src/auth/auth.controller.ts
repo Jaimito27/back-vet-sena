774ea55c29e4ee29c8f118e06f6e5ed756d4ec79
Jaimito27 <softjaimito27@gmail.com>
2024-08-01 10:08:23 -0500

    Se instala bcryptjs para encriptar la contraseña

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+
+    constructor(
+        private readonly authService: AuthService
+    ){}
+
+    @Post('register')
+    register(){
+        return 'register'
+    }
+
+
+    @Post('login')
+    login(){
+        return this.authService.login()
+    }
+}
