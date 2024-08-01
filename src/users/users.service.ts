32dd23af5fc31972fdbc681b6f5710a7110bdd36
Jaimito27 <softjaimito27@gmail.com>
2024-08-01 10:22:29 -0500

    se implementa metood de hasheo de contraseña para usuario

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,21 +12,19 @@ import { Repository } from 'typeorm';
 import { LoginService } from '../../src/login/login.service';
 import { Login } from '../../src/login/entities/login.entity';
 
+import * as bcryptjs from 'bcryptjs';
+
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
 
-    private readonly loginService: LoginService
+    private readonly loginService: LoginService,
   ) {}
 
   async createUser(user: CreateUserDto) {
     const userFound = await this.userRepository.findOne({
-      where: [
-        { ident_document: user.ident_document },
-
-        { email: user.email },
-      ],
+      where: [{ ident_document: user.ident_document }, { email: user.email }],
     });
 
     if (userFound) {
@@ -40,63 +43,63 @@ export class UsersService {
       }
     }
 
-const newLogin = await this.loginService.createLogin({
-  username: user.username,
-  password: user.password
-})
-  if(!(newLogin instanceof Login)) return new HttpException('Ya existe un usuario creado con ese npmbre de usuario', HttpStatus.CONFLICT)
-
+    const heshedPassword = await bcryptjs.hash(user.password, 10);
+    const newLogin = await this.loginService.createLogin({
+      username: user.username,
+      password: heshedPassword,
+    });
+    if (!(newLogin instanceof Login))
+      return new HttpException(
+        'Ya existe un usuario creado con ese npmbre de usuario',
+        HttpStatus.CONFLICT,
+      );
 
     const newUser = this.userRepository.create(user);
     newUser.login = newLogin;
     return await this.userRepository.save(newUser);
-
-
   }
 
   async getUsers() {
-    return await this.userRepository.find({relations:['pets','login']});
+    return await this.userRepository.find({ relations: ['pets', 'login'] });
   }
 
   async getOnlyUser(ident_document: string) {
-    const userFound =  await this.userRepository.findOne({
+    const userFound = await this.userRepository.findOne({
       where: {
         ident_document,
       },
-      relations:['pets','login'],
+      relations: ['pets', 'login'],
     });
 
-    if(!userFound){
-      return new  HttpException('Usuario no existe', HttpStatus.NOT_FOUND)
+    if (!userFound) {
+      return new HttpException('Usuario no existe', HttpStatus.NOT_FOUND);
     }
 
     return userFound;
   }
 
   async updateUser(ident_document: string, user: UpdateUserDto) {
-
-
     const userFound = await this.userRepository.findOne({
       where: {
-        ident_document
-      }
-    })
+        ident_document,
+      },
+    });
 
-    if(!userFound){
-      return new HttpException('Usuario no existe', HttpStatus.NOT_FOUND)
+    if (!userFound) {
+      return new HttpException('Usuario no existe', HttpStatus.NOT_FOUND);
     }
 
-    const updateUser = Object.assign(userFound, user) //une la info tanto de los datos que existian como lo que se está ingresando
+    const updateUser = Object.assign(userFound, user); //une la info tanto de los datos que existian como lo que se está ingresando
     return await this.userRepository.save(updateUser);
   }
 
   async removeUser(ident_document: string) {
-    const result = await this.userRepository.delete({ident_document})
+    const result = await this.userRepository.delete({ ident_document });
 
-    if(result.affected === 0){
-      return new HttpException('El usuario no existe', HttpStatus.NOT_FOUND)
+    if (result.affected === 0) {
+      return new HttpException('El usuario no existe', HttpStatus.NOT_FOUND);
     }
-    
+
     return result;
   }
 }
