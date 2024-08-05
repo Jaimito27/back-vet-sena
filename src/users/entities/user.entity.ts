006a4b9d5b8d20b6b8a1a8bb6f112ef4306e5672
Jaimito27 <softjaimito27@gmail.com>
2024-08-05 12:38:06 -0500

    se modifica entidad user con el fin de unificar empleados y usuarios

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,3 @@
-
 import { Pet } from '../../pets/entities/pet.entity';
 import {
   Column,
@@ -15,20 +14,11 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  doc_type: string;
-
   @Column({ nullable: false, unique: true })
-  ident_document: string;
+  username: string;
 
-  @Column({ nullable: false })
-  names: string;
-
-  @Column({ nullable: false })
-  last_name: string;
-
-  @Column({ nullable: false })
-  phone: string;
+  @Column({ nullable: false, length: 8 })
+  password: string;
 
   @Column({ nullable: false, unique: true })
   email: string;
@@ -36,6 +26,11 @@ export class User {
   @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
   creation_date: Date;
 
+  @Column({ default: 'user' })
+  role: string;
+
+  @Column({ default: 'none' })
+  cargo: string;
 
   @OneToMany(() => Pet, (pet) => pet.owner)
   pets: Pet[];
