a935ab9ef9cac63d8c7b46834c9f03a6d269b132
Jaimito27 <softjaimito27@gmail.com>
2024-08-24 23:48:19 -0500

    Se ingresa los roles que puede usar los diferentes metodos en pets

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -2,34 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PetsService } from './pets.service';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
-
+import { Auth } from '../../src/auth/decorators/auth.decorator';
+import { Role } from '../../src/auth/enums/rol.enum';
 
 @Controller('pets')
 export class PetsController {
   constructor(private readonly petsService: PetsService) {}
 
   @Post()
-
+  @Auth(Role.ADMIN, Role.MEDICAL, Role.USER)
   createPet(@Body() pet: CreatePetDto) {
     return this.petsService.createPet(pet);
   }
 
   @Get()
+  @Auth(Role.ADMIN, Role.MEDICAL, Role.USER)
   async getPets() {
     return await this.petsService.getPets();
   }
 
   @Get(':id')
+  @Auth(Role.ADMIN, Role.MEDICAL)
   findOne(@Param('id') id: string) {
     return this.petsService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth(Role.ADMIN, Role.MEDICAL, Role.USER)
   update(@Param('id') id: string, @Body() pet: UpdatePetDto) {
     return this.petsService.updatePet(id, pet);
   }
 
   @Delete(':id')
+  @Auth(Role.ADMIN, Role.MEDICAL, Role.USER)
   remove(@Param('id') id: string) {
     return this.petsService.remove(id);
   }
